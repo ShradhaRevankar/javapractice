434851abfe092b03a22143a2ce0e3e8984445286
ShradhaRevankar <shradhavrevankar@gmail.com>
2023-11-28 20:47:04 +0530

    commiting main class
    . Added main method

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * this is my first code
+ */
+
+public class Main {
+    /**
+     *
+     * @param args -- what input is accepted
+     */
+    public static void main(String[] args) {
+
+
+    }
+
+}
